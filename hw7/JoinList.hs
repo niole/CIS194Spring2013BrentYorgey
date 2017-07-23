32a8d6f1432b498df67666122a10ac26e569496e
niole <niolenelson@gmail.com>
2017-07-23 16:40:00 -0700

    adds dropJ

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -34,13 +34,25 @@ jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty = Nothing
 indexJ _ (Single _ a) = Just a
-indexJ index (Append s left right) = indexJHelper (getSize $ size $ tag left) (getSize $ size s)
-  where indexJHelper sizeM sizeS
+indexJ index (Append s left right) = helper (getSize $ size $ tag left) (getSize $ size s)
+  where helper sizeM sizeS
           | index >= sizeS = Nothing
           | index < sizeM = indexJ index left
           | index >= sizeM = indexJ (index - sizeM) right
 
-x = Append (Size 4) (Append (Size 2) (Single (Size 1) "a") (Single (Size 1) "b")) (Append (Size 2) (Single (Size 1) "c") (Single (Size 1) "d"))
+x = (+++) ( (+++) (Single (Size 1) "a") (Single (Size 1) "b")) ((+++) (Single (Size 1) "c") (Single (Size 1) "d"))
 
---makeJSTree :: (Sized m, Monoid m) => [a] -> JoinList m a
---makeJSTree es = foldl (+++) Empty $ map (\e -> Single (Size 1) e) es
+--2
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ _ Empty = Empty
+dropJ _ (Single a b) = Single a b
+dropJ n (Append s left right) = let sizeL = getSize $ size $ tag left
+                                    sizeR = getSize $ size $ tag right
+                                    sizeS = getSize $ size s
+                                in case () of
+                                  _ | (n - sizeS) == 0 -> Empty
+                                    | (n - sizeL) == 0 -> right
+                                    | (n - sizeL) < 0 -> Append s (dropJ n left) right --recurse into left
+                                    | otherwise -> dropJ (n - sizeL) right --modify n and go right
+
